e7f81c1777fc542a1d0642df8032ebcc86f1fa7e
Korolev Vladislav <v.korolev@g.nsu.ru>
2025-04-25 22:17:28 +0700

    add application configuration and mvc tests

---
+++ service/src/test/groovy/ru/homevault/fileserver/SpringBootSpec.groovy
@@ -0,0 +1,8 @@
+package ru.homevault.fileserver
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+abstract class SpringBootSpec extends Specification {
+}
